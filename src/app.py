af23fc983430d1579dd0286ee72fe3306510f224
kao-hurtado22 <kao.hurtado22@gmail.com>
2022-02-25 04:46:03 +0000

    avance starwars blog api

---
+++ src/app.py
@@ -0,0 +1,178 @@
+from flask import Flask, jsonify, request, json
+from flask_migrate import Migrate
+from models import db, User, Profile
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import (JWTManager, create_access_token, get_jwt_identity, jwt_required)
+import datetime
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['ENV'] = 'development'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///dev_4geeks.db"
+app.config['JWT_SECRET_KEY'] = ''
+
+db.init_app(app)
+Migrate(app, db)
+jwt = JWTManager(app)
+
+
+@app.route('/users', methods=['GET', 'POST'])
+@app.route('/users/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+def users(id = None):
+    if request.method == 'GET':
+        if id is not None:
+            user = User.query.get(id)
+            if not user: return jsonify({ "msg": "User not found!"}), 404
+            return jsonify(user.serilize()), 200
+        else:
+            users = User.query.all()
+            users = list(map(lambda user: user.serialize(), users))
+            return jsonify(users), 200
+
+            """ columna de Characters """
+
+    if request.method == 'POST':
+       
+        name = request.json.get('name')
+        lastname = request.json.get('lastname')
+        specie = request.json.get('specie', "")
+        homeworld = request.json.get('homeworld', "")
+        gender = request.json.get('gender', "")
+        
+
+        if not name: return jsonify({ "msg": "Name is required!"}), 400
+        if not lastname: return jsonify({ "msg": "Lastname is required!"}), 400
+
+        user = User() 
+        user.name = name
+        user.lastname = lastname
+
+        profile = Profile()
+        profile.specie = specie
+        profile.homeworld = homeworld
+        profile.gender = gender   
+
+        user.profile = profile
+        user.save()
+
+        return jsonify(user.serialize()), 201
+
+    if request.method == 'PUT':
+     
+        name = request.json.get('name') 
+        lastname = request.json.get('lastname')
+        specie = request.json.get('specie', "")
+        homeworld = request.json.get('homeworld', "")
+        gender = request.json.get('gender', "")
+
+        if not name: return jsonify({ "msg": "Name is required!"}), 400
+        if not lastname: return jsonify({ "msg": "Lastname is required!"}), 400
+
+        user = User.query.get(id) 
+
+        if not user: return jsonify({ "msg": "User not found!"}), 404
+
+        user.name = name
+        user.lastname = lastname
+        user.profile.specie = specie
+        user.profile.world = world
+        user.profile.gender = gender
+        user.update()
+
+        return jsonify(user.serialize()), 200
+
+        """ Columna de Planets """
+
+  if request.method == 'POST':
+       
+        name = request.json.get('name')
+        terrain = request.json.get('terrain')
+        climate = request.json.get('climate')
+        gravity = request.json.get('gravity')
+        
+        if not name: return jsonify({ "msg": "Name is required!"}), 400
+
+        user = User() 
+        user.name = name
+
+        profile = Profile()
+        profile.terrain = terrain
+        profile.climate = climate
+        profile.gravity = gravity
+         
+        user.profile = profile
+        user.save()
+
+        return jsonify(user.serialize()), 201
+
+    if request.method == 'PUT':
+     
+        name = request.json.get('name')
+        terrain = request.json.get('terrain')
+        climate = request.json.get('climate')
+        gravity = request.json.get('gravity')
+
+        if not name: return jsonify({ "msg": "Name is required!"}), 400
+
+        user = User.query.get(id) 
+
+        if not user: return jsonify({ "msg": "User not found!"}), 404
+
+        user.name = name
+        user.terrain = terrain
+        user.profile.climate = climate
+        user.profile.gravity = gravity
+        user.update()
+
+        return jsonify(user.serialize()), 200
+
+
+        """ columna starships """
+
+    if request.method == 'POST':
+       
+        name = request.json.get('name')
+        model = request.json.get('model')
+        manufacturer = request.json.get('manufacturer')
+        starship_class = request.json.get('starship_class') 
+
+        if not name: return jsonify({ "msg": "Name is required!"}), 400
+
+        user = User() 
+        user.name = name
+
+        profile = Profile()
+        profile.model = model
+        profile.manufacturer = manufacturer
+        profile.starship_class = starship_class
+
+        user.profile = profile
+        user.save()
+
+        return jsonify(user.serialize()), 201
+
+    if request.method == 'PUT':
+     
+        name = request.json.get('name')
+        model = request.json.get('model')
+        manufacturer = request.json.get('manufacturer')
+        starship_class = request.json.get('starship_class')
+
+        if not name: return jsonify({ "msg": "Name is required!"}), 400
+
+        user = User.query.get(id) 
+
+        if not user: return jsonify({ "msg": "User not found!"}), 404
+
+        user.name = name
+        user.profile.model = model
+        user.profile.manufacturer = manufacturer
+        user.profile.starship_class = starship_class
+        user.update()
+
+        return jsonify(user.serialize()), 200
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=3452)
---
+++ src/models.py
@@ -0,0 +1,123 @@
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    __tablename__ = 'User'
+    id =db.Column(db.Integer, primary_key=True)
+    email =  db.Column(db.String(100), unique=true, nullable=False)
+    password =  db.Column(db.String(100), nullable=False)
+    characters = db.relationship('Characters', backref='user', uselist=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            "password": self.password,
+            "characters": self.characters.serialize()
+        }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+class Characters(db.Model):
+    __tablename__ = 'characters'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text(), default="")
+    lastname = db.Column(db.Text(), default="")
+    specie =db.Column(db.Text(), default="")
+    homeworld = db.Column(db.Text(), default="")
+    gender = db.Column(db.Text(), default="")
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    favorites_characters = db.relationship('favorites_characters', backref='characters', uselist=False)
+    
+     def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.user.email,
+            "lastname": self.lastname,
+            "specie": self.specie,
+            "world": self.world,
+            "gender": self.gender,
+            "favorites_characters": self.favorites_characters.serialize()
+        }
+
+class favorites_characters(db.Model):
+    __tablename__ = 'favorites_characters'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    characters_id = db.Column(db.Integer, db.ForeignKey('characters.id'), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id
+        }
+
+class planets(db.Model):
+    __tablename__ = 'planets'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    terrain = db.Column(db.String(100), nullable=False)
+    climate = db.Column(db.String(100), nullable=False)
+    gravity = db.Column(db.String(100), nullable=False)
+    favorites_planets = db.relationship('favorites_planets', backref='planets', uselist=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "world": self.world,
+            "lenguage": self.lenguage,
+            "favorites_plantes": self.favorites_planets.serialize()
+        }
+
+class favorites_planets(db.Model):
+    __tablename__ = 'favorites_planets'
+    id =db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    planets_id = db.Column(db.Integer, db.ForeignKey('planets.id'), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id
+        }
+
+class starships(db.Model):
+    __tablename__ = 'starships'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=false)
+    model = db.Column(db.String(100), nullable=False)
+    manufacturer = db.Column(db.String(100), nullable=False)
+    starship_class = db.Column(db.String(100), nullable=False)
+    favorites_starships = db.relationship('favorites_starships', backref='starships', uselist=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "model": self.model,
+            "manufacturer": self.manufacturer,
+            "starship_class": self.starship_class,
+            "favorites_starships": self.favorites_starships.serialize()
+        }
+
+class favorites_starships(db.Model):
+    __tablename__ = 'favorites_starships'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id =db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    starships_id = db.Column(db.Integer, db.ForeignKey('starships.id'), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id
+        }
+
+
+
